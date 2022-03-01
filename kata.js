557db32fbc1ebcd29357687b9aea3750a32f7935
annasun919 <yuval_sun@yahoo.com.hk>
2022-03-01 22:22:09 +0800

    01/03/2022

--- kata.js
+++ kata.js
@@ -180,3 +180,36 @@ function isIsogram(str) {
 function manhattanDistance(pointA, pointB) {
   return Math.abs(pointA[0] - pointB[0]) + Math.abs(pointA[1] - pointB[1]);
 }
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  const string2 = [];
+  const string1 = words.split(" ");
+
+  const num = words
+    .replace(/[^1-9]/g, "")
+    .split("")
+    .map(Number);
+
+  let j = 1;
+
+  while (string2.length != num.length) {
+    for (let i = 0; i < num.length; i++) {
+      if (j === num[i]) {
+        string2.push(string1[i]);
+      }
+    }
+    j++;
+  }
+  return string2.join(" ");
+}
